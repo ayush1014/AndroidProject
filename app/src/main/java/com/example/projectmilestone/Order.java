622d201de48d11d5b16f47c1205be2390dd79602
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2021-04-27 14:59:21 -0500

    Added codes to make app work. Added recycler view in Listing. Added database to save previous orders.

--- app/src/main/java/com/example/projectmilestone/Order.java
+++ app/src/main/java/com/example/projectmilestone/Order.java
@@ -12,71 +12,62 @@ import android.widget.Toast;
 
 public class Order extends AppCompatActivity {
 
+    String type,toppings;
+    int price;
+    TextView tv_total,tv_toppings,tv_type;
+    EditText number;
+    Button order_btn;
+    DBHelper dbHelper;
+    Intent i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-    }
 
-    public static class Order extends AppCompatActivity {
+        init();
+        gettingIntent();
+        settingValues();
 
-        String type,toppings;
-        int price;
-        TextView tv_total,tv_toppings,tv_type;
-        EditText number;
-        Button order_btn;
-        DBHelper dbHelper;
-        Intent i;
+        dbHelper = new DBHelper(this);
+        i = new Intent(this,Listing.class);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_order);
-
-            init();
-            gettingIntent();
-            settingValues();
-
-            dbHelper = new DBHelper(this);
-            i = new Intent(this,Listing.class);
-
-            order_btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (number.getText().toString().isEmpty()){
-                        Toast.makeText(com.example.projectmilestone.Order.this, "Please enter 919 number", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        dbHelper.insert(type,price,toppings);
-                        Toast.makeText(com.example.projectmilestone.Order.this, "Order created successfully", Toast.LENGTH_SHORT).show();
-                        startActivity(i);
-                    }
-
+        order_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (number.getText().toString().isEmpty()){
+                    Toast.makeText(Order.this, "Please enter 919 number", Toast.LENGTH_SHORT).show();
                 }
-            });
+                else{
+                    dbHelper.insert(type,price,toppings);
+                    Toast.makeText(Order.this, "Order created successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(i);
+                }
 
-        }
-
-        private void settingValues() {
-            tv_total.setText("Your total is: "+price);
-            tv_toppings.setText(toppings);
-            tv_type.setText("Type: "+type);
-        }
-
-        private void gettingIntent() {
-            type = getIntent().getStringExtra("type");
-            price = getIntent().getIntExtra("price",0);
-            toppings = getIntent().getStringExtra("toppings");
-        }
-
-        public void init(){
-            tv_total = findViewById(R.id.textView_total);
-            number = findViewById(R.id.editText_number);
-            tv_type = findViewById(R.id.type);
-            tv_toppings = findViewById(R.id.toppings);
-            order_btn = findViewById(R.id.order_btn);
-        }
-
+            }
+        });
 
     }
+
+    private void settingValues() {
+        tv_total.setText("Your total is: "+price);
+        tv_toppings.setText(toppings);
+        tv_type.setText("Type: "+type);
+    }
+
+    private void gettingIntent() {
+        type = getIntent().getStringExtra("type");
+        price = getIntent().getIntExtra("price",0);
+        toppings = getIntent().getStringExtra("toppings");
+    }
+
+    public void init(){
+        tv_total = findViewById(R.id.textView_total);
+        number = findViewById(R.id.editText_number);
+        tv_type = findViewById(R.id.type);
+        tv_toppings = findViewById(R.id.toppings);
+        order_btn = findViewById(R.id.order_btn);
+    }
+
+
 }
